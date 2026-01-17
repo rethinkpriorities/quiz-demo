9f36236eab32facea1bb7e34f0d792749c095b0c
Manu Meixell <misternu@gmail.com>
2026-01-17 01:17:47 -0800

    Test pre-commit hook

---
+++ test-linting.jsx
@@ -0,0 +1,6 @@
+// Test file for linting - intentionally bad formatting
+const badFunction = () => {
+  const unused = 'this variable is not used';
+  return <div>Test</div>;
+};
+export default badFunction;
