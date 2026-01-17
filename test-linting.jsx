9e043782637072a969738d2ed56c831f1bd7f55f
Manu Meixell <misternu@gmail.com>
2026-01-17 01:18:05 -0800

    Remove test file

--- test-linting.jsx
+++
@@ -1,6 +0,0 @@
-// Test file for linting - intentionally bad formatting
-const badFunction = () => {
-  const unused = 'this variable is not used';
-  return <div>Test</div>;
-};
-export default badFunction;
